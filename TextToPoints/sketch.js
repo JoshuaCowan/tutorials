a6b67c6ab012759c73991766eb29c49c2875aacb
JoshuaCowan <suede53@gmail.com>
2017-07-02 12:11:00 -0400

    First commit

---
+++ TextToPoints/sketch.js
@@ -0,0 +1,53 @@
+// Daniel Shiffman
+// http://codingtra.in
+// Steering Text Paths
+// Video: https://www.youtube.com/watch?v=4hA7G3gup-4
+
+var font;
+var vehicles = [];
+
+function preload() {
+  font = loadFont('arial.ttf');
+}
+
+function setup() {
+  createCanvas(1000, 300);
+  background(51);
+  // textFont(font);
+  // textSize(192);
+  // fill(255);
+  // noStroke();
+  // text('train', 100, 200);
+
+  var points = font.textToPoints('Lyla_and_James', 100, 200, 96, {
+    sampleFactor: 0.25
+  });
+
+  for (var i = 0; i < points.length; i++) {
+    var pt = points[i];
+    var vehicle = new Vehicle(pt.x, pt.y);
+    vehicles.push(vehicle);
+    // stroke(255);
+    // strokeWeight(8);
+    // point(pt.x, pt.y);
+  }
+}
+
+function draw() {
+  background(0,200,255);
+    
+    noStroke();
+    fill(244,244,66);
+    ellipse(140,40,80,80);
+    
+    noStroke();
+    fill(35,163,38);
+    rect(0, this.height, this.width, -60 );
+    
+  for (var i = 0; i < vehicles.length; i++) {
+    var v = vehicles[i];
+    v.behaviors();
+    v.update();
+    v.show();
+  }
+}
